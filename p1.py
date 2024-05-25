015f257a1e610a2466faf5fdd87ced82814aa290
DayOS9 <velozity9@gmail.com>
2024-05-25 02:33:14 -0700

    following vid, very simple single neuron calc

--- p1.py
+++ p1.py
@@ -1,7 +1,6 @@
-import sys
-import numpy as np
-import matplotlib
+inputs = [1, 2, 3]
+weights = [0.2, 0.8, -0.5]
+bias = 2
 
-print("Python:", sys.version)
-print("Numpy:", np.__version__)
-print("Matplotlib: ", matplotlib.__version__)
+output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias
+print(output)
