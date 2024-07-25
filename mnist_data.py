0fe629b0b6d5173fc0b3ac980c8ec9354fec09ef
DayOS <79074357+DayOS9@users.noreply.github.com>
2024-07-25 15:43:01 -0700

    comments over where the data package was found and how it is opened and formatted

--- mnist_data.py
+++ mnist_data.py
@@ -3,39 +3,16 @@ import pickle
 
 import numpy as np
 
-"""import seaborn as sb
-from matplotlib import pyplot as plt
-from scipy.io import loadmat"""
-
-# opening the data obtaining the actual values and the labels
-"""mnist = loadmat("./mnist-original.mat")
-mnist_data = mnist["data"].T
-mnist_label = mnist["label"][0]"""
-
-
-"""image_size_px = int(np.sqrt(mnist_data.shape[1]))
-print("\u2705 [Info] The images size is (", image_size_px, "x", image_size_px, ")")"""
-
-
-"""def mnist_random_example():
-    idx = np.random.randint(70000)
-    exp = mnist_data[idx].reshape(image_size_px, image_size_px)
-    print("\u2705 [Info] The number in the image below is:", mnist_label[idx])
-    plt.imshow(exp, cmap=sb.color_palette("mako", as_cmap=True))
-    plt.show()"""
-
-
-"""def data():
-    # split the data into different sets
-    data_dev = mnist_data[0:10000]
-    dev_labels = mnist_label[0:10000]
-    print(data_dev.shape)
-    print(dev_labels)"""
-
 
 def load_data():
+    """Although there are many sites where the mnist_data can be downloaded from, this specific file was taken from
+    Michael Nielsen https://github.com/mnielsen/neural-networks-and-deep-learning.git"""
     with gzip.open("./mnist.pkl.gz", "rb") as f:
-        training_data, validation_data, test_data = pickle.load(f, encoding="latin1")
+        """each variable will take on a tuple where the first entry will contain a list of the images and the second entry will contain
+        a list of the labels"""
+        training_data, validation_data, test_data = pickle.load(
+            f, encoding="latin1"
+        )  # encoding set to latin1 since utf did not work
         return training_data, validation_data, test_data
 
 
@@ -50,18 +27,13 @@ def correct_output(x):
 def data_wrapper():
     # here we are going to reshape the data to be in the correct dimensions for us to manipulate
     training_data, validation_data, test_data = load_data()
+    # the 28x28 images will be transformed into 784x1 numpy array (kinda like a matrix/vector)
     training_input = [np.reshape(x, (784, 1)) for x in training_data[0]]
     training_result = [correct_output(x) for x in training_data[1]]
+    # here we zip them up again to their original format as being a tuple with the first entry being the data, and the second the labels
     training_data = zip(training_input, training_result)
     validation_input = [np.reshape(x, (784, 1)) for x in validation_data[0]]
     validation_data = zip(validation_input, validation_data[1])
     test_input = [np.reshape(x, (784, 1)) for x in test_data[0]]
     test_data = zip(test_input, test_data[1])
     return (training_data, validation_data, test_data)
-
-
-data_wrapper()
-# print(mnist_data[0].reshape(784, 1).dot(2))
-
-
-# mnist_random_example()
