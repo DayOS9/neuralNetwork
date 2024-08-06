950a3b588df5ad1379b8f332d25c8692193a8e8a
DayOS <79074357+DayOS9@users.noreply.github.com>
2024-08-05 21:29:23 -0700

    added the additional argument so that the drawing pop up can show up

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ import net
 
 training_data, validation_data, test_data = mnist_data.data_wrapper()
 network = net.Network([784, 30, 10])
-network.SGD(training_data, 30, 10, 0.1, test_data=test_data)
+network.SGD(training_data, 30, 10, 0.1, test_data=test_data, drawer=True)
